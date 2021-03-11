a75e3663ad974635d0afa7078ae58b8cfb9395a3
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-03-11 20:04:08 +0100

    🔧 add base next.js configuration

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import Layout from '../components/Layout'
+
+const Index = (): JSX.Element => (
+    <Layout title="Home">
+      <h1>xD</h1>
+    </Layout>
+  )
+
+export default Index
