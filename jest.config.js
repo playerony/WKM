aff69784168b4ee439abe9dd6b6377aa7978f26a
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-03-13 19:37:25 +0100

    ✅ configure jest

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  globals: {
+    'ts-jest': {
+      tsConfig: './tsconfig.json'
+    }
+  },
+  moduleFileExtensions: ['js', 'ts', 'tsx'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  },
+  testMatch: ['**/*.test.(ts|js|tsx)'],
+  testEnvironment: 'node',
+  preset: 'ts-jest',
+  setupFilesAfterEnv: [
+    '<rootDir>/setupEnzyme.ts',
+  ],
+}
