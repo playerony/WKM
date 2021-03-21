0edb3b4e4934bb7567e8a2353ad09dab2a1d2c57
Paweł Wojtasiński <p.wojtasinski@exlabs.pl>
2021-03-21 14:31:20 +0100

    cleanup

--- src/components/common/ImageModal/ImageModal.component.tsx
+++ src/components/common/ImageModal/ImageModal.component.tsx
@@ -3,13 +3,10 @@ import React from 'react'
 import { StyledModal, StyledImage } from './ImageModal.styles'
 import { ImageModalProps } from './ImageModal.types'
 
-const ImageModal = ({ src, ...restProps }: ImageModalProps): JSX.Element => {
-
-  return (
-    <StyledModal width="90%" centered={true} footer={null} {...restProps}>
-      <StyledImage src={src || ''} />
-    </StyledModal>
-  )
-}
+const ImageModal = ({ src, ...restProps }: ImageModalProps): JSX.Element => (
+  <StyledModal width="90%" centered={true} footer={null} {...restProps}>
+    <StyledImage src={src || ''} />
+  </StyledModal>
+)
 
 export default ImageModal
